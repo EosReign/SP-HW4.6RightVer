755c3564d73a987fe52e50c879ec315d4e479857
EosReign <vsevolod_sol@mail.ru>
2022-12-14 02:43:17 +0300

    meh

---
+++ src/main/java/prosky/sphw46rightver/Application.java
@@ -0,0 +1,13 @@
+package prosky.sphw46rightver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+}
